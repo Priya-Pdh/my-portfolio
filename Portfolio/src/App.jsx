c92701200f9b56e171ddcdb0aeb699deff03ca1e
Priya Pradhan <102596886+Priya-Pdh@users.noreply.github.com>
2023-12-18 19:27:58 +0100

    initialize project

---
+++ Portfolio/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+
+
+export const App = () => {
+  return (
+    <>
+    <h1>Hello</h1>
+     <p>a</p>
+      
+      </>
+
+  );
+};
---
+++ Portfolio/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,30 @@
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About Me</Link>
+        </li>
+        <li>
+          <Link to="/project">Project</Link>
+        </li>
+        <li>
+          <Link to="/education">Education</Link>
+        </li>
+        <li>
+          <Link to="/skills">Skills</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
