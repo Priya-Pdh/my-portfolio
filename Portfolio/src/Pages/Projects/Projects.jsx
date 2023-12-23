3938fc987653d62519ebc2f20f79eab61faa1a00
Priya Pradhan <102596886+Priya-Pdh@users.noreply.github.com>
2023-12-23 09:53:33 +0100

    styling project page

--- Portfolio/src/Pages/Projects/Projects.jsx
+++ Portfolio/src/Pages/Projects/Projects.jsx
@@ -1,24 +1,39 @@
+import { Box, Typography, Card, CardContent, CardMedia, Button } from '@mui/material';
 import data from '../../data.json';
 
 function Project() {
-    const { projects } = data;
+  const { projects } = data;
+
   return (
-    <section className="my-8">
+    <Box my={4}>
       {projects.map((project, index) => (
-        <div key={index} >
-          <h2>{project.name}</h2>
-          <p>{project.description}</p>
-          <img src={project.image} alt={project.name} className="w-full h-auto rounded-md" style={{width: "50%"} } />
-          <a
-            href={project.link}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Click to view the project
-          </a>
-        </div>
+        <Card key={index} sx={{ maxWidth: 500, margin: '0 auto 20px' }}>
+          <CardMedia
+            component="img"
+            alt={project.name}
+            height="auto"
+            image={project.image}
+          />
+          <CardContent>
+            <Typography variant="h5" component="div" gutterBottom>
+              {project.name}
+            </Typography>
+            <Typography variant="body2" color="text.secondary" paragraph>
+              {project.description}
+            </Typography>
+            <Button
+              href={project.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              variant="contained"
+              color="primary"
+            >
+              Click to view the project
+            </Button>
+          </CardContent>
+        </Card>
       ))}
-    </section>
+    </Box>
   );
 }
 
