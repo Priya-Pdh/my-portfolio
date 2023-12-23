e47dda1ec34bc6c54d39f538bf91c2b1b3eaefb0
Priya Pradhan <102596886+Priya-Pdh@users.noreply.github.com>
2023-12-23 10:09:58 +0100

    updates in education page and styling

--- Portfolio/src/Pages/Education/Education.jsx
+++ Portfolio/src/Pages/Education/Education.jsx
@@ -1,31 +1,35 @@
+import React from 'react';
+import { Box, Typography, Paper, Link } from '@mui/material';
 import data from '../../data.json';
+
 function Education() {
-    const { education} = data;
-    return (
-      <section >
-        <h2 >Education</h2>
-        {education.map((edu, index) => (
-          <div key={index}>
-            <div >
-              <img src={edu.logo} alt={edu.name}  />
-              <div>
-                <p >{edu.name}</p>
-                <p >{edu.subject}</p>
-              </div>
-            </div>
-            <a
-              href={edu.website}
-              target="_blank"
-              rel="noopener noreferrer"
-              
-            >
-              Visit {edu.name}
-            </a>
-          </div>
-        ))}
-      </section>
-    );
-  }
-  
-  export default Education;
-  
+  const { education } = data;
+
+  return (
+    <Box my={4}>
+      <Typography variant="h4" gutterBottom>
+        Education
+      </Typography>
+      {education.map((edu, index) => (
+        <Paper key={index} elevation={3} sx={{ padding: 2, marginBottom: 2 }}>
+          <Box display="flex" alignItems="center">
+            <img src={edu.logo} alt={edu.name} style={{ marginRight: '16px', width: '120px', height: '90px' }} />
+            <Box>
+              <Typography variant="h6" gutterBottom>
+                {edu.name}
+              </Typography>
+              <Typography variant="body1" color="text.secondary">
+                {edu.subject}
+              </Typography>
+            </Box>
+          </Box>
+          <Link href={edu.website} target="_blank" rel="noopener noreferrer" color="primary" underline="hover">
+            Visit {edu.name}
+          </Link>
+        </Paper>
+      ))}
+    </Box>
+  );
+}
+
+export default Education;
