a428ee4a860cb4756c0f9376af566c8ec35cd189
Priya Pradhan <102596886+Priya-Pdh@users.noreply.github.com>
2023-12-29 19:03:10 +0100

    styling updates

--- Portfolio/src/Pages/Projects/Projects.jsx
+++ Portfolio/src/Pages/Projects/Projects.jsx
@@ -1,4 +1,6 @@
+import React from 'react';
 import { Box, Typography, Card, CardContent, CardMedia, Button, Paper, Grid } from '@mui/material';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import data from '../../data.json';
 
 function Project() {
@@ -6,29 +8,35 @@ function Project() {
 
   return (
     <Box my={4}>
-      {projects.map((project, index) => (
-        <Paper key={index} sx={{ margin: '0 auto 20px', maxWidth: 800 }}>
-          <Grid container spacing={2}>
-  
-              <CardMedia component="img" alt={project.name} height="auto" image={project.image} />
-          
-          
-                <Typography variant="h5" component="div" gutterBottom>
-                  {project.name}
+      {projects.map(({ name, image, description, link }, index) => (
+        <Paper key={index} sx={{ margin: '0 auto 40px', maxWidth: '90%', padding: '20px' }}>
+          <Grid container spacing={3} alignItems="center" justifyContent="center">
+            {/* Project Image in the Center (xs) */}
+            <Grid item xs={12} md={8} sx={{ textAlign: 'center' }}>
+              <CardMedia component="img" alt={name} height="auto" maxWidth="100%" image={image} sx={{ boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)' }} />
+            </Grid>
+
+            {/* Project Description and Button in the Center (xs, md) */}
+            <Grid item xs={12} md={8} sx={{ textAlign: 'center' }}>
+              <CardContent>
+                <Typography variant="h5" gutterBottom>
+                  {name}
                 </Typography>
                 <Typography variant="body2" color="text.secondary" paragraph>
-                  {project.description}
+                  {description}
                 </Typography>
                 <Button
-                  href={project.link}
+                  href={link}
                   target="_blank"
                   rel="noopener noreferrer"
                   variant="contained"
                   color="primary"
+                  endIcon={<ArrowForwardIcon />}
                 >
                   Click to view the project
                 </Button>
-           
+              </CardContent>
+            </Grid>
           </Grid>
         </Paper>
       ))}
--- Portfolio/src/components/NavBar/NavBar.jsx
+++ Portfolio/src/components/NavBar/NavBar.jsx
@@ -1,56 +1,118 @@
-import React from 'react';
-import styled from 'styled-components';
+import { useState } from 'react';
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Box,
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+} from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
+import HomeIcon from '@mui/icons-material/Home';
+import WorkIcon from '@mui/icons-material/Work';
+import SchoolIcon from '@mui/icons-material/School';
+import EmojiObjectsIcon from '@mui/icons-material/EmojiObjects';
+import ContactMailIcon from '@mui/icons-material/ContactMail';
 import { Link } from 'react-router-dom';
 
-const StyledNavbar = styled.nav`
-  background-color: #333;
-`;
-
-const StyledUl = styled.ul`
-  list-style-type: none;
-  margin: 0;
-  padding: 0;
-  display: flex;
-  justify-content: flex-end;
-`;
-
-const StyledLi = styled.li`
-  margin-left: 15px;
-`;
-
-const StyledLink = styled(Link)`
-  display: block;
-  color: white;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-
-  &:hover {
-    background-color: #555;
-  }
-`;
-
 const Navbar = () => {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const toggleDrawer = (open) => () => {
+    setDrawerOpen(open);
+  };
+
+  const navItems = [
+    { to: '/', icon: <HomeIcon />, label: 'Home' },
+    { to: '/project', icon: <WorkIcon />, label: 'Project' },
+    { to: '/education', icon: <SchoolIcon />, label: 'Education' },
+    { to: '/skills', icon: <EmojiObjectsIcon />, label: 'Skills' },
+    { to: '/contact', icon: <ContactMailIcon />, label: 'Contact' },
+  ];
+
   return (
-    <StyledNavbar>
-      <StyledUl>
-        <StyledLi>
-          <StyledLink to="/">Home</StyledLink>
-        </StyledLi>
-        <StyledLi>
-          <StyledLink to="/project">Project</StyledLink>
-        </StyledLi>
-        <StyledLi>
-          <StyledLink to="/education">Education</StyledLink>
-        </StyledLi>
-        <StyledLi>
-          <StyledLink to="/skills">Skills</StyledLink>
-        </StyledLi>
-        <StyledLi>
-          <StyledLink to="/contact">Contact</StyledLink>
-        </StyledLi>
-      </StyledUl>
-    </StyledNavbar>
+    <AppBar position="static" style={{ backgroundColor: 'transparent',  borderRadius: "15px" }}>
+      <Toolbar >
+        <Typography variant="h6" component="div"  sx={{
+            flexGrow: 1,
+            background: 'linear-gradient(45deg, #ff199f, #cd8cdd, #5be288, #bef146, #7e6ed8)',
+            WebkitBackgroundClip: 'text',
+            color: 'transparent',
+            fontWeight: 600,
+            fontSize: '25px',
+            
+            
+          }}> Welcome to Priya's Portfolio
+        </Typography>
+        <Box sx={{ display: { xs: 'block', md: 'none' } }}>
+          <IconButton
+            size="large"
+            edge="start"
+            color="black"
+            aria-label="menu"
+            onClick={toggleDrawer(true)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Drawer
+            anchor="right"
+            open={drawerOpen}
+            onClose={toggleDrawer(false)}
+            sx={{ width: '100vw' }}
+          >
+            <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+              <IconButton onClick={toggleDrawer(false)}>
+                <CloseIcon />
+              </IconButton>
+            </Box>
+            <List>
+              {navItems.map((item, index) => (
+                <Link
+                  key={index}
+                  to={item.to}
+                  style={{ textDecoration: 'none', color: 'black' }}
+                  onClick={toggleDrawer(false)}
+                >
+                  <ListItem>
+                    <ListItemIcon>{item.icon}</ListItemIcon>
+                    <ListItemText primary={item.label} />
+                  </ListItem>
+                </Link>
+              ))}
+            </List>
+          </Drawer>
+        </Box>
+        <Box
+          sx={{
+            display: { xs: 'none', md: 'flex', gap: '10px' },
+            marginRight: '2rem', // Adjust the margin as needed
+          }}
+        >
+          {navItems.map((item, index) => (
+            <Link
+              key={index}
+              to={item.to}
+              style={{ textDecoration: 'none', color: 'white', marginLeft: '15px' }}
+            >
+              <IconButton
+                size="large"
+                edge="start"
+                color="black"
+                aria-label={item.label}
+                sx={{ '&:hover': { border: "1px solid black" } }}
+              >
+                {item.icon}
+              </IconButton>
+            </Link>
+          ))}
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
 };
 
