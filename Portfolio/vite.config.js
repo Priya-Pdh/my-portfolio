05915809b51b3c4a100be8b7ffdeec9c80437147
Priya Pradhan <102596886+Priya-Pdh@users.noreply.github.com>
2024-01-09 10:41:08 +0100

    updated the vite.config.js file

--- Portfolio/vite.config.js
+++ Portfolio/vite.config.js
@@ -1,5 +1,10 @@
-export default {
+import React from 'react';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
   build: {
     outDir: 'dist',  // This should match the "publish" directory in netlify.toml
   },
-}
+});
