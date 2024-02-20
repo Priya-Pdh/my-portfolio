61f931829d1c9ed41918af4b1b60418c569ffefd
Priya Pradhan <102596886+Priya-Pdh@users.noreply.github.com>
2024-02-20 15:06:46 +0100

    changes the contact card

--- Portfolio/src/Pages/Contact/Contact.jsx
+++ Portfolio/src/Pages/Contact/Contact.jsx
@@ -8,62 +8,69 @@ function Contact() {
 
   return (
     <Box my={4} textAlign="center" sx={{ marginTop: { sm: "50px", md: "80px", lg: '100px'}}}>
-      <Grid container justifyContent="center"  >
+      <Grid container justifyContent="center">
         <Grid item xs={12} sm={8} md={6} lg={4}>
-          <Card elevation={3} sx={{ margin: 'auto', width: '100%',  backgroundColor: '#f8f8f8' }}>
-            <CardContent>
-              <Typography variant="h4" mb={2} sx= {{fontSize: {xs: "20px", sm:" 26px", md: "28px", lg: "30px"}}}>
-                Contact Information 📞
-                <Divider />
-              </Typography>
-              <List>
-                <ListItem>
-                  <ListItemIcon>
-                    <FaMapMarkerAlt />
-                  </ListItemIcon>
-                  <ListItemText primary={`Location: ${contactInfo.location}`} primaryTypographyProps={{ color: 'primary' }} />
-                </ListItem>
-                <ListItem>
-                  <ListItemIcon>
-                    <FaEnvelope />
-                  </ListItemIcon>
-                  <ListItemText primary={`Email: ${contactInfo.email}`} primaryTypographyProps={{ color: 'primary' }} />
-                </ListItem>
-                <ListItem>
-                  <ListItemIcon>
-                    <FaLinkedin />
-                  </ListItemIcon>
-                  <ListItemText>
-                    <Link
-                      href={contactInfo.linkedin}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      color="primary"
-                      underline="hover"
-                    >
-                      LinkedIn
-                    </Link>
-                  </ListItemText>
-                </ListItem>
-                <ListItem>
-                  <ListItemIcon>
-                    <FaGithub />
-                  </ListItemIcon>
-                  <ListItemText>
-                    <Link
-                      href={contactInfo.github}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      color="primary"
-                      underline="hover"
-                    >
-                      Github
-                    </Link>
-                  </ListItemText>
-                </ListItem>
-              </List>
-            </CardContent>
-          </Card>
+          <Box
+            border="0 solid #ccc"
+            borderRadius="30px"
+            p="30px"
+            boxShadow="0px 4px 10px rgba(0, 0, 0, 0.1)"
+          >
+            <Card elevation={0} sx={{ backgroundColor: 'transparent' }}>
+              <CardContent>
+                <Typography variant="h4" mb={2} sx={{ fontSize: { xs: "20px", sm: "26px", md: "28px", lg: "30px" } }}>
+                  Contact Information 📞
+                  <Divider />
+                </Typography>
+                <List>
+                  <ListItem>
+                    <ListItemIcon>
+                      <FaMapMarkerAlt />
+                    </ListItemIcon>
+                    <ListItemText primary={`Location: ${contactInfo.location}`} primaryTypographyProps={{ color: 'primary' }} />
+                  </ListItem>
+                  <ListItem>
+                    <ListItemIcon>
+                      <FaEnvelope />
+                    </ListItemIcon>
+                    <ListItemText primary={`Email: ${contactInfo.email}`} primaryTypographyProps={{ color: 'primary' }} />
+                  </ListItem>
+                  <ListItem>
+                    <ListItemIcon>
+                      <FaLinkedin />
+                    </ListItemIcon>
+                    <ListItemText>
+                      <Link
+                        href={contactInfo.linkedin}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        color="primary"
+                        underline="hover"
+                      >
+                        LinkedIn
+                      </Link>
+                    </ListItemText>
+                  </ListItem>
+                  <ListItem>
+                    <ListItemIcon>
+                      <FaGithub />
+                    </ListItemIcon>
+                    <ListItemText>
+                      <Link
+                        href={contactInfo.github}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        color="primary"
+                        underline="hover"
+                      >
+                        Github
+                      </Link>
+                    </ListItemText>
+                  </ListItem>
+                </List>
+              </CardContent>
+            </Card>
+          </Box>
         </Grid>
       </Grid>
     </Box>
